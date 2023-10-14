0174ec8bb4ec5baef36c2ced4ebdd9d8ebb68881
ColdWindScholar <3590361911@qq.com>
2023-10-14 14:09:25 +0800

    init

---
+++ bin/getSuperSize.py
@@ -0,0 +1,20 @@
+def main(device):
+    device = device.upper()
+    if device in ['FUXI', 'NUWA', 'ISHTAR']:
+        return 9663676416
+    elif device in ['MARBLE', 'SOCRATES', 'BABYLON']:
+        return 9663676416
+    elif device == 'SUNSTONE':
+        return 9122611200
+    elif device == 'YUDI':
+        return 11811160064
+    else:
+        return 9126805504
+# pipa 9126805504 |Pad6
+# liuqin 9126805504 |Pad6Pro
+# sunstone 9126805504 or 9122611200 |Note 12 5G
+# rembrandt 9126805504 |K60E
+# redwood 9126805504 |Note12ProSpeed
+# mondrian 9126805504 |K60
+# yunluo 9126805504 |RedmiPad
+# ruby 9126805504 |Note 12 Pro
---
+++ port.py
@@ -0,0 +1,13 @@
+# miui_port project
+
+# Only For V-A/B Device
+
+# Based on Android 13
+
+# Test Base ROM: Mi 10S (V14.0.6)
+
+# Test Port ROM: Mi13、Mi13Pro、Mi13Ultra
+
+# 底包和移植包为外部参数传入
+
+import sys
