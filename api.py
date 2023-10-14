21fa1ea0b83dd87b781b26fca52c7a5196f8b601
ColdWindScholar <3590361911@qq.com>
2023-10-14 22:31:16 +0800

    init

--- api.py
+++ api.py
@@ -1,3 +1,6 @@
+import os
+
+
 def getprop(name, path):
     with open(path, 'r', encoding='utf-8') as prop:
         for s in prop.readlines():
@@ -6,3 +9,14 @@ def getprop(name, path):
             if name in s:
                 return s.strip().split('=')[1]
     return ''
+
+
+def findfile(file, dir_) -> str:
+    for root, dirs, files in os.walk(dir_, topdown=True):
+        if file in files:
+            if os.name == 'nt':
+                return (root + os.sep + file).replace("\\", '/')
+            else:
+                return root + os.sep + file
+        else:
+            return ''
